61fe68b45a65ab1944ab4f68ee930750771e7f64
lianianm email = lianlianm19@gmail.com <you@example.com>
2020-10-21 16:35:15 -0700

    updated module 7 note

--- ma.lianlian/index.php
+++ ma.lianlian/index.php
@@ -1,1 +1,26 @@
-hello
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Store</title>
+    <?php include "parts/meta.php" ?>
+</head>
+
+<body>
+    <?php include "parts/navbar.php" ?>
+
+
+
+
+    <div class="view-window" style="background-image:url(img/xiaomi002.jpg)">
+        <h2>Product Name</h2>
+    </div>
+
+
+
+    <div class="container">
+        <div class="card soft">
+            <h2>Welcome to the Store</h2>
+        </div>
+    </div>
+</body>
+</html>
--- ma.lianlian/parts/navbar.php
+++ ma.lianlian/parts/navbar.php
@@ -1,14 +1,13 @@
 <header class="navbar">
 		<div class="container display-flex flex-align-center">
 			<div class="flex-none">
-				<h1>XIAOMI</h1>
+				<h1>Store Website</h1>
 			</div>
             <div class="flex-stretch"></div>
 			<nav class="nav flex-none">
 				<ul class="display-flex">
-					<li><a href="#article1">About</a></li>
-					<li><a href="#article2">Reward</a></li>
-					<li><a href="#article3">Details</a></li>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="product_list.php">Store</a></li>
 				</ul>
 			</nav>
 		</div>
