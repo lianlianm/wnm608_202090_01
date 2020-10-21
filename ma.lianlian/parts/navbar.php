e16eca470ae9d502042f9b5147d79fccac17050e
lianianm email = lianlianm19@gmail.com <you@example.com>
2020-10-21 16:08:03 -0700

    updated module 7 note

---
+++ ma.lianlian/parts/navbar.php
@@ -0,0 +1,15 @@
+<header class="navbar">
+		<div class="container display-flex flex-align-center">
+			<div class="flex-none">
+				<h1>XIAOMI</h1>
+			</div>
+            <div class="flex-stretch"></div>
+			<nav class="nav flex-none">
+				<ul class="display-flex">
+					<li><a href="#article1">About</a></li>
+					<li><a href="#article2">Reward</a></li>
+					<li><a href="#article3">Details</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
