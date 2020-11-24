d597d24317abe60efc6dbf01567b9b39acfc6d27
lianianm email = lianlianm19@gmail.com <you@example.com>
2020-11-23 23:52:41 -0800

    recording notes for module 7

--- ma.lianlian/demo/learning_php.php
+++ ma.lianlian/demo/learning_php.php
@@ -115,37 +115,45 @@ echo "<{$_GET['type']}>My name is {$_GET['name']}</{$_GET['type']}>";
 
 // Arrays 数组
 // [] 等同于 array(),都是创建一个数组，array(key => value, ...)
+// 所以，下面两个变量设置，是一样的
+
 $colors =  array("red","green", "blue");
 $colors = ["red","green","blue"]; 
 
-echo $colors[1]; // red    $color[这其中是要输入数值，从0开始]
+echo $colors[1]; // green    $color[这其中是要输入数值，从0开始]
 
 echo "
-    <br>$colors[0];
-    <br>$colors[1];
+    <br>$colors[0]
+    <br>$colors[1]
     <br>$colors[2]
 ";  
 // red
 // green
-// blue
+// blue有转行
+echo "
+    $colors[0]
+    $colors[1]
+    $colors[2]
+"; 
+//redgreenblue 无转行无space
 
-
-
-echo count($colors); // 3
+echo count($colors); // 3 count is a function函数
 
 ?>
  
 
 <div style="color:<?= $colors[1] ?>">
     This text is green
-</div> 
+</div>
+<!-- php中<?= ?> ,这个tag是意思echo out one value that you put inside of this block -->
 <!-- 将green颜色代入 -->
 
 
 <?php
 
-// Associative Array 联合数组
-// => 用在数组中，是指对应关系，相当于value
+// Associative Array 联合数组(是带有字符串索引的数组，array with string indexes
+// )
+// => equals arrow等号箭头 是用在数组中，是指对应关系，相当于value，讲此值分配给该索引
 
 $colorsAssociative = [
    "red" => "#f00",
@@ -161,21 +169,22 @@ echo $colorsAssociative['red']; // #foo
 
 <?php
 
-// Casting
-$c = "$a";
-$d = $c*1;  
+// Casting 转换
+// 将一个元素的值类型转换我另一种值类型
+$c = "$a"; //设置变量c为字符串string 5 而不是number 5，
+$d = $c*1;  // 将string 5乘以1，就强制把string5 转换成呢个number5
 
 $colorsObject = (object)$colorsAssociative;
 
-// echo $colorsObject;
+// echo $colorsObject; 不运行的，无法直接echo对象
 
 
 echo "<hr>";
 
-// Array Index Notation
+// Array Index Notation 数组索引符号
 echo $colors[0]."<br>"; // red
 echo $colorsAssociative['red']."<br>"; // #foo
-echo $colorsAssociative[$colors[0]]."<br>"; // #foo
+echo $colorsAssociative[$colors[0]]."<br>"; // #foo 使用变量来获取变量，使用变量来创建变量
 
 // Object Property Notation ->
 // -> 是指对象属性符号，比如下面是从$colorObject中调用red
@@ -192,17 +201,19 @@ echo $colorsObject->{$colors[0]}."<br>"; // #foo
 
 <?php
 
-//PHP中动态输出HTML内容，是通过print 和 echo 语句来实现的，在实际使用中， print 和 echo 两者的功能几乎是完全一样。 但， echo可以同时输出多个字符串，而print只可以同时输出一个字符串
+//PHP中动态输出HTML内容，是通过print 和 echo 语句来实现的，在实际使用中， print 和 echo 两者的功能几乎是完全一样。 但， echo可以同时输出多个字符串，而print_r只可以同时输出一个字符串
 
 print_r($colors); // Array([0]=>red[1]=>green[2]=>blue)
 echo "<hr>";
+
 print_r($colorsAssociative); // Array([red]=>#foo[green]=>#ofo[blue]=>#oof)
 echo "<hr>";
+
 echo "<pre>",print_r($colorsObject),"</pre>"; // html的<pre></pre>标签用可定义预格式化的文本。 被包围在pre 元素中的文本通常会保留空格和换行符。 而文本也会呈现为等宽字体。 
 
 
 
-// CUSTOM FUNCTIONS 自创建function
+// CUSTOM FUNCTIONS 自创建函数function
 // 如下就是自定义一个print_p功能
 
 function print_p($d) {
